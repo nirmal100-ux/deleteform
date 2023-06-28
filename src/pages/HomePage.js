a8a1dd4346ef2104972ed5452a4492f0c30a74e0
nirmal100-ux <nirmalstha783@gmail.com>
2023-06-28 07:52:47 +0545

    chas

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { removeData } from "../features/infoSlice";
+import { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
 const HomePage = () => {
 
 
   const { infos } = useSelector((store) => store.infos);
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
 
   const nav = useNavigate();
   return (
+
     <div className="grid grid-cols-4 gap-5 p-4">
 
       {infos.map((info, i) => {
@@ -34,9 +47,29 @@ const HomePage = () => {
             <div className=" flex justify-end px-2 py-1 space-x-5">
               <button onClick={() => nav(`/update/${info.id}`)} ><i className="fa-solid fa-pen-to-square fa-lg"></i></button>
 
-              <button onClick={() => dispatch(removeData(i))} ><i class="fa-solid fa-trash"></i>
 
-              </button>
+              <Button onClick={handleOpen}>
+                <i class="fa-solid fa-trash"></i>
+              </Button>
+              <Dialog open={open} handler={handleOpen}>
+                <DialogHeader>Delete.</DialogHeader>
+                <DialogBody divider>
+                  You want to Delete
+                </DialogBody>
+                <DialogFooter>
+                  <Button
+                    variant="text"
+                    color="red"
+                    onClick={handleOpen}
+                    className="mr-1"
+                  >
+                    <span>Cancel</span>
+                  </Button>
+                  <Button variant="gradient" color="red" onClick={handleOpen}>
+                    <span onClick={() => dispatch(removeData(i))}>Confirm</span>
+                  </Button>
+                </DialogFooter>
+              </Dialog>
 
 
 
@@ -48,6 +81,7 @@ const HomePage = () => {
 
 
     </div >
+
   )
 }
 export default HomePage
